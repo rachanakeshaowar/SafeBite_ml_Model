056bf8568a233ca33cb050e5197072c52bc890d1
rachanakeshaowar <keshrachana0@gmail.com>
2025-04-29 23:06:40 +0530

    added imports

---
+++ py.py
@@ -0,0 +1,20 @@
+from huggingface_hub import notebook_login
+
+notebook_login()
+
+
+import re
+import json
+import torch
+import logging
+from PIL import Image
+from symspellpy import SymSpell, Verbosity
+import pytesseract
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline, TextStreamer
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+IMAGE_PATH = "/content/Maggie.jpg" # Replace with your image path
+
+
+MODEL_NAME = "mistralai/Mistral-7B-Instruct-v0.3"
