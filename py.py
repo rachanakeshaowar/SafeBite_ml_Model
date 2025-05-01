2d5e7204344d106923d4823f05121b93cb6b6b11
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-01 10:49:03 +0530

    ingredient cleaning function

--- py.py
+++ py.py
@@ -82,6 +82,7 @@ def clean_ingredients(text):
          for ing in potential_ingredients:
             cleaned_ing = re.sub(r"^[^\w(]+|[^\w)]+$", "", ing.strip()).strip() # Allow starting '(' and ending ')'
             cleaned_ing = cleaned_ing.replace(' :', '') # Remove stray colons
+            if cleaned_ing and len(cleaned_ing) > 1 and not cleaned_ing.isdigit():
          
          
          
