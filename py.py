e7c208ab2046ff4fdf9b9ffa4f849320daff5290
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-01 10:52:38 +0530

    ingredient cleaning function

--- py.py
+++ py.py
@@ -87,6 +87,7 @@ def clean_ingredients(text):
                if sym_spell:
                   suggestions = sym_spell.lookup(cleaned_ing, Verbosity.CLOSEST, max_edit_distance=SYM_SPELL_EDIT_DISTANCE, include_unknown=True)
                   if suggestions and suggestions[0].distance < SYM_SPELL_EDIT_DISTANCE + 1: # Only correct if close enough
+                     best_suggestion = suggestions[0].term
                
          
          
