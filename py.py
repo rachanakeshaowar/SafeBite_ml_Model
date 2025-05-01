e63c1d3122981a7c3f335849c9a42d4b4d5c95f1
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-01 11:09:09 +0530

    ingredient cleaning function

--- py.py
+++ py.py
@@ -111,7 +111,9 @@ def load_model():
     logging.info(f"Loading model: {MODEL_NAME}")
     try: 
         quantization_config = BitsAndBytesConfig(
-            load_in_4bit=True,          
+            load_in_4bit=True,
+            bnb_4bit_quant_type="nf4",
+            bnb_4bit_compute_dtype=torch.bfloat16          
          
          
          
